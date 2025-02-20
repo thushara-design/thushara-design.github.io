a360fde691e1f6c077071f5b6cd2af92a25c835d
Devanshu <design.devanshu@gmail.com>
2025-02-21 00:22:40 +0530

    refactor: reorganize image imports and add new landing SVG assets

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,17 +1,19 @@
-import arrowDownToLine from "../assets/arrow-down-to-line.svg";
-import flower from "../assets/flower.svg";
 import arrowDown from "../assets/arrow-down.svg";
+import arrowDownToLine from "../assets/arrow-down-to-line.svg";
 import arrowRight from "../assets/arrow-right.svg";
-import profile from "../assets/profile.png";
-// import profile from "../assets/image.png";
-import Linkedin from "../assets/linkedin";
-import Mail from "../assets/mail";
-import Menu from "./menu";
+import ArrowRight from "./arrow-right";
 import close from "../assets/close.svg";
+import flower from "../assets/flower.svg";
 import heroBG1 from "../assets/hero-bg-1.svg";
 import heroBG2 from "../assets/hero-bg-2.svg";
+import landingLeft from "../assets/landing-left.svg";
+import landingRight from "../assets/landing-right.svg";
+import Linkedin from "../assets/linkedin";
 import Logo from "./logo";
-import ArrowRight from "./arrow-right";
+import Mail from "../assets/mail";
+import Menu from "./menu";
+// import profile from "../assets/image.png";
+import profile from "../assets/profile.png";
 import subtractLeft from "../assets/subtract-left.svg";
 import subtractRight from "../assets/subtract-right.svg";
 
@@ -25,6 +27,8 @@ export {
   flower,
   heroBG1,
   heroBG2,
+  landingLeft,
+  landingRight,
   Linkedin,
   Logo,
   Mail,
