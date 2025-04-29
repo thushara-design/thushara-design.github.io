91af480d17eb7b4eec6ec72ed51b9257bbe051c3
thushara-design <thusharavarghese9@gmail.com>
2025-04-29 11:08:11 +0530

    Use temporary image for Aria project

--- src/data/case-studies.ts
+++ src/data/case-studies.ts
@@ -51,7 +51,7 @@ const caseStudies = [
     tagline: "PRESENTATION",
     tags: ["Career Pivoters", "Design", "Research"],
     description: "Aria is a collaborative project that aims to help career pivoters find their next career opportunity.",
-    image: "case-study-4.png",
+    image: "case-study-1.png",
     role: "Product Designer",
     timeframe: "In Progress",
     tools: ["Figma", "Notion"],
