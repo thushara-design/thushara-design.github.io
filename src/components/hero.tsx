d7e04ea2933557a2c15d9ba1c35ad58938445efe
thushara-design <thusharavarghese9@gmail.com>
2025-05-07 10:01:22 +0530

    Change hero fonts to Nunito

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -6,8 +6,8 @@ export const Hero = () => {
   return (
     <AnimatedSection id="#" className="flex flex-col items-center font-body md:flex-row md:py-16">
       <div className="mb-16 mt-28 flex flex-col md:mb-0 md:w-full">
-        <h1 className="leading-20 font-title text-5xl font-medium sm:text-6xl">Hi, I&apos;m Thushara.</h1>
-        <p className="my-8 font-title text-3xl leading-10">
+        <h1 className="leading-20 font-sans text-5xl font-medium sm:text-6xl">Hi, I&apos;m Thushara.</h1>
+        <p className="my-8 font-sans text-3xl leading-10">
           I design intuitive digital experiences that bring simplicity, clarity, and ease to users while aligning with business goals.
         </p>
         <a href={socialLinks.find(({ label }) => label === "Email")?.link}>
