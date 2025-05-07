5b3029f4ab51311af84bf1d73d3e64886c29ec15
thushara-design <thusharavarghese9@gmail.com>
2025-05-07 09:58:26 +0530

    Update hero bio text

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -8,8 +8,7 @@ export const Hero = () => {
       <div className="mb-16 mt-28 flex flex-col md:mb-0 md:w-full">
         <h1 className="leading-20 font-title text-5xl font-medium sm:text-6xl">Hi, I&apos;m Thushara.</h1>
         <p className="my-8 font-title text-3xl leading-10">
-          I'm a <em className="italic">UX/UI Designer</em> passionate about creating intuitive, empathetic design solutions that simplify user tasks
-          while aligning with both user needs and business objectives.
+          I design intuitive digital experiences that bring simplicity, clarity, and ease to users while aligning with business goals.
         </p>
         <a href={socialLinks.find(({ label }) => label === "Email")?.link}>
           <Button variant="outline">Work with me</Button>
